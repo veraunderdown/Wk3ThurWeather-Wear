7f2d0f858c551b213c25d546d2312413a0d976d7
Oleg Gorecan <rvo@btinternet.com>
2017-11-22 17:06:00 +0000

    Add project files.

---
+++ Wk3ThurWeather-Wear/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3ThurWeather_Wear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature;
+            int humidity;
+            int windspeed;
+            int cloudcover;
+
+            Console.Write("Enter temperature: ");
+            temperature = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter humidity: ");
+            humidity = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter windspeed: ");
+            windspeed = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter cloudcover: ");
+            cloudcover = int.Parse(Console.ReadLine());
+
+            if((temperature > 20) && (windspeed < 30));
+            Console.Write("Wear swimwear!");
+            
+            if (cloudcover < 5) ;
+            Console.Write("Wear a hat!");
+            
+            if((humidity > 70) && (windspeed < 10));
+            Console.Write("Take an umbrella!");
+            
+            else 
+            Console.Write("Wear a raincoat!");
+            
+            if ((temperature > 15) && (temperature < 20));
+            Console.Write("Wear shorts!"); 
+
+            else if (temperature <= 15)
+                Console.Write("Wear trousers!");
+
+        }
+    }
+}
